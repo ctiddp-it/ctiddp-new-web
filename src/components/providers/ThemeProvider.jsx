8d38a06fe7978cd68e257a35220c0d862e2895ff
B.K.S.S.S MALLIK <sssmallik143@gmail.com>
2026-04-28 11:43:52 +0530

    Changed the website default theme to dark mode.

--- src/components/providers/ThemeProvider.jsx
+++ src/components/providers/ThemeProvider.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 export default function ThemeProvider({ children }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="light" enableSystem>
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       {children}
     </NextThemesProvider>
   )
